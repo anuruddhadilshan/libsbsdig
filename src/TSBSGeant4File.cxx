3aadd809134c3bffba984e79cdf15486a9f45807
Eric Fuchey <efuchey@jlab.org>
2018-08-06 10:32:08 -0400

    Added in TSBSGeant4File the filling of the hits for all detectors.

--- src/TSBSGeant4File.cxx
+++ src/TSBSGeant4File.cxx
@@ -151,32 +151,119 @@ Int_t TSBSGeant4File::ReadNextEvent(int d_flag){
   TVector3 Vtx_MCtrack;
   TVector3 Pos_det;
   int origvolflag;
-  double nph = 0;
   
   double hit_data_temp[19];
   double gen_data_temp[9];
+
+  // Electron Arm
   
-  // Process Scint data
+  // Process GRINCH data
+  if(fTree->Earm_GRINCH.nhits){
+    for(int i = 0; i<fTree->Earm_GRINCH.nhits; i++){
+      g4sbshitdata *grinchhit = new g4sbshitdata(GRINCH_UNIQUE_DETID, 4);
+      grinchhit->SetData(0, fTree->Earm_GRINCH.PMT->at(i));
+      grinchhit->SetData(1, 1);
+      grinchhit->SetData(2, fTree->Earm_GRINCH.NumPhotoelectrons->at(i));
+      grinchhit->SetData(3, fTree->Earm_GRINCH.Time_avg->at(i));
+      fg4sbsHitData.push_back(grinchhit);
+    }
+  }
+    
+  // Process hodoscope data
   if(fTree->Earm_BBHodoScint.nhits){
     for(int i = 0; i<fTree->Earm_BBHodoScint.nhits; i++){
-      g4sbshitdata *hodoscinthit = new g4sbshitdata(HODO_UNIQUE_DETID, 3);
+      g4sbshitdata *hodoscinthit = new g4sbshitdata(HODO_UNIQUE_DETID, 4);
       hodoscinthit->SetData(0, fTree->Earm_BBHodoScint.cell->at(i));
       hodoscinthit->SetData(1, 1);
       hodoscinthit->SetData(2, fTree->Earm_BBHodoScint.sumedep->at(i));
+      hodoscinthit->SetData(3, fTree->Earm_BBHodoScint.tavg->at(i));
       fg4sbsHitData.push_back(hodoscinthit);
       
       for(int j = 0; j<2; j++){//j = 0: close beam PMT, j = 1: far beam PMT
-	nph = 1.0e7*fTree->Earm_BBHodoScint.sumedep->at(i)*0.113187*exp(-(0.3+pow(-1, j)*fTree->Earm_BBHodoScint.xhit->at(i))/1.03533);
-	g4sbshitdata *hodopmthit = new g4sbshitdata(HODO_UNIQUE_DETID, 3);
+	Npe = 1.0e7*fTree->Earm_BBHodoScint.sumedep->at(i)*0.113187*exp(-(0.3+pow(-1, j)*fTree->Earm_BBHodoScint.xhit->at(i))/1.03533)* 0.24;
+	t = fTree->Earm_BBHodoScint.tavg->at(i)+(0.55+pow(-1, j)*fTree->Earm_BBHodoScint.xhit->at(i))/0.15;
+	g4sbshitdata *hodopmthit = new g4sbshitdata(HODO_UNIQUE_DETID, 4);
 	hodopmthit->SetData(0, fTree->Earm_BBHodoScint.cell->at(i)*2+j);
 	hodopmthit->SetData(1, 0);
-	hodopmthit->SetData(2, nph*0.24);
+	hodopmthit->SetData(2, Npe*0.24);
+	hodopmthit->SetData(3, t);
 	fg4sbsHitData.push_back(hodopmthit);
       }
     }
   }
 
-
+  // Process BB PS data
+  if(fTree->Earm_BBPSTF1.nhits){
+    for(int i = 0; i<fTree->Earm_BBPSTF1.nhits; i++){
+      g4sbshitdata *bbpstf1hit = new g4sbshitdata(BBPS_UNIQUE_DETID, 4);
+      bbpstf1hit->SetData(0, fTree->Earm_BBPSTF1.cell->at(i));
+      bbpstf1hit->SetData(1, 1);
+      bbpstf1hit->SetData(2, fTree->Earm_BBPSTF1.sumedep->at(i));
+      bbpstf1hit->SetData(3, fTree->Earm_BBPSTF1.tavg->at(i));
+      fg4sbsHitData.push_back(bbpstf1hit);
+    }
+  }
+  
+  if(fTree->Earm_BBPS.nhits){
+    for(int i = 0; i<fTree->Earm_BBPS.nhits; i++){
+      g4sbshitdata *bbpshit = new g4sbshitdata(BBPS_UNIQUE_DETID, 4);
+      bbpshit->SetData(0, fTree->Earm_BBPS.PMT->at(i));
+      bbpshit->SetData(1, 0);
+      bbpshit->SetData(2, fTree->Earm_BBPS.NumPhotoelectrons->at(i));
+      bbpshit->SetData(3, fTree->Earm_BBPS.Time_avg->at(i));
+      fg4sbsHitData.push_back(bbpshit);
+    }
+  }
+  
+  // Process BB SH data
+  if(fTree->Earm_BBSHTF1.nhits){
+    for(int i = 0; i<fTree->Earm_BBSHTF1.nhits; i++){
+      g4sbshitdata *bbshtf1hit = new g4sbshitdata(BBSH_UNIQUE_DETID, 4);
+      bbshtf1hit->SetData(0, fTree->Earm_BBSHTF1.cell->at(i));
+      bbshtf1hit->SetData(1, 1);
+      bbshtf1hit->SetData(2, fTree->Earm_BBSHTF1.sumedep->at(i));
+      bbshtf1hit->SetData(3, fTree->Earm_BBSHTF1.tavg->at(i));
+      fg4sbsHitData.push_back(bbshtf1hit);
+    }
+  }
+  
+  if(fTree->Earm_BBSH.nhits){
+    for(int i = 0; i<fTree->Earm_BBSH.nhits; i++){
+      g4sbshitdata *bbshhit = new g4sbshitdata(BBSH_UNIQUE_DETID, 4);
+      bbshhit->SetData(0, fTree->Earm_BBSH.PMT->at(i));
+      bbshhit->SetData(1, 0);
+      bbshhit->SetData(2, fTree->Earm_BBSH.NumPhotoelectrons->at(i));
+      bbshhit->SetData(3, fTree->Earm_BBSH.Time_avg->at(i));
+      fg4sbsHitData.push_back(bbshhit);
+    }
+  }
+  
+  
+  // Hadron Arm
+  // Process CDet data
+  if(fTree->Harm_CDET_Scint.nhits){
+    for(int i = 0; i<fTree->Harm_CDET_Scint.nhits; i++){
+      g4sbshitdata *cdetscinthit = new g4sbshitdata(CDET_UNIQUE_DETID, 4);
+      cdetscinthit->SetData(0, fTree->Harm_CDET_Scint.cell->at(i));
+      cdetscinthit->SetData(1, 1);
+      cdetscinthit->SetData(2, fTree->Harm_CDET_Scint.sumedep->at(i));
+      cdetscinthit->SetData(3, fTree->Harm_CDET_Scint.tavg->at(i));
+      fg4sbsHitData.push_back(cdetscinthit);
+    }
+  }
+  // For the time being, use the g4sbs npe estimation for CDET, divided by 5.
+  // (the p.e. yield for cosmics form g4sbs is about 5 times more the one measured)
+  if(fTree->Harm_CDET.nhits){
+    for(int i = 0; i<fTree->Harm_CDET.nhits; i++){
+      g4sbshitdata *cdetpmthit = new g4sbshitdata(CDET_UNIQUE_DETID, 4);
+      cdetpmthit->SetData(0, fTree->Harm_CDET.PMT->at(i));
+      cdetpmthit->SetData(1, 0);
+      cdetpmthit->SetData(2, round(fTree->Harm_CDET.NumPhotoelectrons->at(i)/5.0) );
+      cdetpmthit->SetData(3, fTree->Harm_CDET.Time_avg->at(i));
+      fg4sbsHitData.push_back(cdetpmthit);
+    }
+  }
+  
   // Now process HCAL data
   if(fTree->hcalpart.E) {
     for(size_t k = 0; k < fTree->hcalpart.E->size(); k++) {
