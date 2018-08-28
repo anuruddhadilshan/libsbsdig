ade46fdd381f58e125751cf906acfe8cb8f01dd5
Juan Carlos Cornejo <cornejo@jlab.org>
2018-08-28 13:01:24 -0400

    Changed function from Add to AddDetector to match SimDigitizer

--- example/digi_hcal_test.C
+++ example/digi_hcal_test.C
@@ -43,7 +43,7 @@ void digi_hcal_test()
   TSBSGeant4File *f = new TSBSGeant4File("data/sbsin_931.root");
   
   TSBSSimHCal *hcal = new TSBSSimHCal();
-  digitizer->Add(hcal);
+  digitizer->AddDetector(hcal);
 
   // Now start the digitization on this file
   digitizer->Process(f);
