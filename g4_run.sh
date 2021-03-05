ca5c917e5c7a65c87c233d6bff35ccf5576437c3
Thir Narayan Gautam <gautam@ifarm1901.jlab.org>
2021-03-05 12:43:27 -0500

    GEn-RP variables pushed into master

--- g4_run.sh
+++ g4_run.sh
@@ -3,5 +3,5 @@ pwdd=`pwd`
 #rm -r genrp_4.5GeV2_conf3_1.root
 cd ../../g4_work/build/
 #g4sbs preinit_ckov_noscint_nocalorimeters.mac genrp_4.5GeV2.mac
-cp -r genrp_4.5GeV2_conf3.root /w/halla-scifs17exp/sbs/thir/vgit4libsbsdg/libsbsdig-1/genrp_4.5GeV2_conf3_3.root
+cp -r genrp_4.5GeV2_conf3.root /w/halla-scifs17exp/sbs/thir/git_master/libsbsdig/genrp_4.5GeV2_conf3_3.root
 cd $pwdd
--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 pwdd=`pwd`
 ./g4_run.sh
-cd /w/halla-scifs17exp/sbs/thir/vgit4libsbsdg/buildM/
+cd /w/halla-scifs17exp/sbs/thir/git_master/build_NN/
 rm -r *
-cmake -DCMAKE_INSTALL_PREFIX=/w/halla-scifs17exp/sbs/thir/vgit4libsbsdg/libsbsdig-install/ ../libsbsdig-1/ && make install
+cmake -DCMAKE_INSTALL_PREFIX=/w/halla-scifs17exp/sbs/thir/git_master/libsbsdig-install/ /w/halla-scifs17exp/sbs/thir/git_master/libsbsdig/ && make install
 echo $pwdd
 cd $pwdd
 sbsdig db/db_genrp_conf_dev.dat gmn13.5_elastic_ex.txt 1000
