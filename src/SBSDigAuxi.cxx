678551f24489578ddbf64b373cfbd7c9ec96de59
Thir Narayan Gautam <gautam@ifarm1901.jlab.org>
2021-06-17 14:36:42 -0400

    GEN-rp GEMs: applied z and y offsets to the side GEMs

--- src/SBSDigAuxi.cxx
+++ src/SBSDigAuxi.cxx
@@ -638,10 +638,10 @@ bool UnfoldData(g4sbs_tree* T, double theta_sbs, double d_hcal, TRandom3* R,
 	  hit.edep = T->Harm_PrPolGEMBeamSide.edep->at(k)*1.0e9;//eV! not MeV!!!!
 	  hit.t = tzero+T->Harm_PrPolGEMBeamSide.t->at(k);
 	  hit.xin = T->Harm_PrPolGEMBeamSide.xin->at(k)-gemdets[idet]->GEMPlanes[mod*2].Xoffset();
-	  hit.yin = T->Harm_PrPolGEMBeamSide.zin->at(k)-gemdets[idet]->GEMPlanes[mod*2+1].Xoffset();
+	  hit.yin = T->Harm_PrPolGEMBeamSide.zin->at(k)-gemdets[idet]->GEMPlanes[mod*2+1].Xoffset()-1.36;
 	  hit.zin = T->Harm_PrPolGEMBeamSide.yin->at(k)-gemdets[idet]->fZLayer[T->Harm_PrPolGEMBeamSide.plane->at(k)-1];//+0.8031825;
 	  hit.xout = T->Harm_PrPolGEMBeamSide.xout->at(k)-gemdets[idet]->GEMPlanes[mod*2].Xoffset();
-	  hit.yout = T->Harm_PrPolGEMBeamSide.zout->at(k)-gemdets[idet]->GEMPlanes[mod*2+1].Xoffset();
+	  hit.yout = T->Harm_PrPolGEMBeamSide.zout->at(k)-gemdets[idet]->GEMPlanes[mod*2+1].Xoffset()-1.36;
 	  hit.zout = T->Harm_PrPolGEMBeamSide.yout->at(k)-gemdets[idet]->fZLayer[T->Harm_PrPolGEMBeamSide.plane->at(k)-1];//+0.8031825;
 	  gemdets[idet]->fGEMhits.push_back(hit);
 //cout<<" Harm_PrPolGEMBeamSide  "<<"  zin  "<<hit.zin<<"  zout  "<<hit.zout<<" plane "<<T->Harm_PrPolGEMBeamSide.plane->at(k)<<endl;
@@ -680,10 +680,10 @@ bool UnfoldData(g4sbs_tree* T, double theta_sbs, double d_hcal, TRandom3* R,
 	  hit.edep = T->Harm_PrPolGEMFarSide.edep->at(k)*1.0e9;//eV! not MeV!!!!
 	  hit.t = tzero+T->Harm_PrPolGEMFarSide.t->at(k);
 	  hit.xin = T->Harm_PrPolGEMFarSide.xin->at(k)-gemdets[idet]->GEMPlanes[mod*2].Xoffset();
-	  hit.yin = T->Harm_PrPolGEMFarSide.zin->at(k)-gemdets[idet]->GEMPlanes[mod*2+1].Xoffset();
+	  hit.yin = T->Harm_PrPolGEMFarSide.zin->at(k)-gemdets[idet]->GEMPlanes[mod*2+1].Xoffset()-1.36;
 	  hit.zin = T->Harm_PrPolGEMFarSide.yin->at(k)-gemdets[idet]->fZLayer[T->Harm_PrPolGEMFarSide.plane->at(k)-1];//+0.8031825;
 	  hit.xout = T->Harm_PrPolGEMFarSide.xout->at(k)-gemdets[idet]->GEMPlanes[mod*2].Xoffset();
-	  hit.yout = T->Harm_PrPolGEMFarSide.zout->at(k)-gemdets[idet]->GEMPlanes[mod*2+1].Xoffset();
+	  hit.yout = T->Harm_PrPolGEMFarSide.zout->at(k)-gemdets[idet]->GEMPlanes[mod*2+1].Xoffset()-1.36;
 	  hit.zout = T->Harm_PrPolGEMFarSide.yout->at(k)-gemdets[idet]->fZLayer[T->Harm_PrPolGEMFarSide.plane->at(k)-1];//+0.8031825;
 	  gemdets[idet]->fGEMhits.push_back(hit);
 	  //cout<<" Harm_PrPolGEMFarSide  "<<"  zin  "<< T->Harm_PrPolGEMFarSide.yin->at(k)-gemdets[idet]->fZLayer[T->Harm_PrPolGEMFarSide.plane->at(k)-1]<<"  zout   "<<T->Harm_PrPolGEMFarSide.yout->at(k)-gemdets[idet]->fZLayer[T->Harm_PrPolGEMFarSide.plane->at(k)-1]<<endl;
